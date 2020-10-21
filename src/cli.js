7a45148049616937b15f28a327f69aa3660a08d3
German <pentagram777@mail.ru>
2020-10-21 11:39:13 +0500

    feat: add step four

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,5 @@ import promptly from 'promptly';
 
 (async () => {
   const name = await promptly.prompt('May I have your name?');
-  console.log(`Nice to meet you, ${name}`)
-}) ();
-
+  console.log(`Nice to meet you, ${name}`);
+})();
