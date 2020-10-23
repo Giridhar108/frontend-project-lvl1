0b62424d476e117d2153068687166c2b9b83ce14
German <pentagram777@mail.ru>
2020-10-23 07:07:22 +0500

    feat: logic

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import promptly from 'promptly';
-
-(async () => {
+/* import promptly from 'promptly'; */
+/*
+(async function name() {
   const name = await promptly.prompt('May I have your name?');
   console.log(`Nice to meet you, ${name}`);
-})();
+}()); */
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import promptly from 'promptly';
+
+export default async function head(logic, rule, ifAnswers) {
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Nice to meet you, ${name}`);
+  rule();
+
+  let i = 0;
+  do {
+    const num = logic();
+    console.log(`Question ${num}`);
+    const answer = await promptly.prompt('You answer:');
+    if (answer === ifAnswers(num)) {
+      console.log('correct!');
+      i += 1;
+      if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${ifAnswers(num)}'.\nLet\'s try again, ${name}!`);
+      i += 3;
+    }
+  } while (i <= 2);
+}
